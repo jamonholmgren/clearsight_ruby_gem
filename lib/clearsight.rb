be256b73e7a226fd1308e0aec99f56325365675b
Jamon Holmgren <jamon@clearsightstudio.com>
2013-10-21 14:44:46 -0700

    Progress bar

--- lib/clearsight.rb
+++ lib/clearsight.rb
@@ -1,6 +1,36 @@
 require "clearsight/version"
 require "clearsight/deploy"
+require 'thread'
 
 module Clearsight
-  # Your code goes here...
+  class Timer
+    def initialize(interval, &handler)
+      @run = true
+      @semaphore = Mutex.new
+
+      @th = Thread.new do
+        t = Time.now
+        while run?
+          t += interval
+          (sleep(t - Time.now) rescue nil) and handler.call rescue nil
+        end
+      end
+    end
+
+    def stop
+      @semaphore.synchronize do
+        @run = false
+      end
+      @th.join
+    end
+
+    private
+
+    def run?
+      @semaphore.synchronize do
+        @run
+      end
+    end
+  end
 end
+::CS = ::Clearsight
